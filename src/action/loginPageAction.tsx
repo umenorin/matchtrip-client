898c4d8288881fa5ab5a7c1018b3f7bb6a33447a
Ruan Costa Oliveira <umenorin@gmail.com>
2025-03-31 19:44:23 -0300

    removing the console checkout

--- src/action/loginPageAction.tsx
+++ src/action/loginPageAction.tsx
@@ -5,13 +5,13 @@ export async function loginPageAction({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
   const email = String(formData.get("email"));
   const password = String(formData.get("password"));
+
   const user = {
     email:email,
     password:password
   }
-  // Verifique se a URL está correta
+
   const apiUrl = `${import.meta.env.VITE_LOCAL_API}/api/users/login`;
-  console.log("Enviando para:", apiUrl);
 
   try {
     const response = await axios.post(
@@ -25,25 +25,12 @@ export async function loginPageAction({ request }: ActionFunctionArgs) {
       }
     );
 
-    console.log("Resposta do servidor:", response.data);
-
     if (response.data.token) {
-      return redirect("/dashboard");
+      return redirect("/");
     }
 
   } catch (error: any) {
-    console.error("Erro completo:", error);
-
-    // Verifique se há resposta do servidor
-    if (error.response) {
-      console.error("Dados da resposta de erro:", error.response.data);
-      console.error("Status do erro:", error.response.status);
-      console.error("Headers do erro:", error.response.headers);
-    } else if (error.request) {
-      console.error("A requisição foi feita mas não houve resposta");
-    } else {
-      console.error("Erro ao configurar a requisição:", error.message);
-    }
+    console.error("Error: ", error);
 
   }
 }
--- src/action/registerPageAction.tsx
+++ src/action/registerPageAction.tsx
@@ -13,11 +13,8 @@ export async function registerPageAction({ request }: ActionFunctionArgs) {
     nationality: String(formData.get("nationality")),
     gender: String(formData.get("gender")),
   };
-  console.log(user.uniqueIdentification, "CPF");
-  console.log(user.numberPhone, "TELEFONE");
-  // Verifique se a URL está correta
+
   const apiUrl = `${import.meta.env.VITE_LOCAL_API}/api/users/singup`;
-  console.log("Enviando para:", apiUrl);
 
   try {
     const response = await axios.post(
@@ -31,23 +28,10 @@ export async function registerPageAction({ request }: ActionFunctionArgs) {
       }
     );
 
-    console.log("Resposta do servidor:", response.data);
-
     if (response.data.token) {
-      return redirect("/dashboard");
+      return redirect("/"); 
     }
   } catch (error: any) {
-    console.error("Erro completo:", error);
-
-    // Verifique se há resposta do servidor
-    if (error.response) {
-      console.error("Dados da resposta de erro:", error.response.data);
-      console.error("Status do erro:", error.response.status);
-      console.error("Headers do erro:", error.response.headers);
-    } else if (error.request) {
-      console.error("A requisição foi feita mas não houve resposta");
-    } else {
-      console.error("Erro ao configurar a requisição:", error.message);
-    }
+    console.error("error: ", error);
   }
 }
