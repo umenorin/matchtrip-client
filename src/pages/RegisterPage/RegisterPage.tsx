9ebdf056c7799e40e138fe9ccf85edc0837c0d32
lucas-dev98 <l.o.bastos@live.com>
2025-05-17 10:14:18 -0300

    Modify register page styles and struture

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,85 +1,71 @@
-import { Form, Link } from 'react-router';
-import './RegisterPage.scss';
+import { Form, Link } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 import Input from '../../components/Input/Input';
-import backGrounOpacity from '../../../public/backgroundOpacity.png';
+import './RegisterPage.scss';
 
 export default function RegisterPage() {
   return (
     <div className="register-page">
-      <h1 className="register-page__title">Cadastro</h1>
+      <h1 className="register-page__title">Crie sua conta</h1>
       <div className="formulario">
         <Form className="register-page__form" method="post">
-          {/* Nome Completo */}
-          <Input name="name" type="text" placeholder="Nome Completo" required />
-
-          {/* Email */}
-          <Input name="email" type="email" placeholder="Email" required />
-          
-                    {/* CPF/Identificação Única */}
-                    <Input
-                      name="uniqueIdentification"
-                      type="text"
-                      placeholder="CPF (somente números)"
-                      title="Digite um CPF válido (11 dígitos)"
-                      required
-                    />
-
-          {/* Telefone */}
-          <Input
-            name="numberPhone"
-            type="tel"
-            placeholder="Número de Telefone (com DDD)"
-            title="Digite um número com DDD (ex: 11999999999)"
-          />
-
-          {/* Senha */}
-          <Input
-            name="password"
-            type="password"
-            placeholder="Senha (mínimo 8 caracteres)"
-            minLength="8"
-            required
-          />
-
-
-
-          {/* Nacionalidade */}
-          <Input
-            name="nationality"
-            type="text"
-            placeholder="Nacionalidade (ex: BR)"
-            pattern="[A-Za-z]{2}"
-            title="Código de 2 letras (ex: BR)"
-            required
-          />
-
-          {/* Idade */}
-          <Input
-            name="age"
-            type="number"
-            placeholder="Idade"
-            min="18"
-            max="120"
-            required
-          />
-          {/* Gênero */}
-          <select name="gender" required className="container__input">
-            <option value="">Selecione o gênero</option>
-            <option value="male">Masculino</option>
-            <option value="female">Feminino</option>
-            <option value="other">Outro</option>
-            <option value="prefer-not-to-say">Prefiro não dizer</option>
-          </select>
+          <div className="page__form-inputs">
+            <Input
+              name="name"
+              type="text"
+              placeholder="Nome Completo"
+              required
+            />
+            <Input name="email" type="email" placeholder="Email" required />
+            <Input
+              name="uniqueIdentification"
+              type="text"
+              placeholder="CPF (somente números)"
+              title="Digite um CPF válido (11 dígitos)"
+              required
+            />
+            <Input
+              name="numberPhone"
+              type="tel"
+              placeholder="Número de Telefone (com DDD)"
+              title="Digite um número com DDD (ex: 11999999999)"
+            />
+            <Input
+              name="password"
+              type="password"
+              placeholder="Senha (mínimo 8 caracteres)"
+              minLength={8}
+              required
+            />
+            <Input
+              name="nationality"
+              type="text"
+              placeholder="Nacionalidade (ex: BR)"
+              pattern="[A-Za-z]{2}"
+              title="Código de 2 letras (ex: BR)"
+              required
+            />
+            <Input
+              name="age"
+              type="number"
+              placeholder="Idade"
+              min={18}
+              max={120}
+              required
+            />
+            <select name="gender" required className="container__input">
+              <option value="">Selecione o gênero</option>
+              <option value="male">Masculino</option>
+              <option value="female">Feminino</option>
+              <option value="other">Outro</option>
+              <option value="prefer-not-to-say">Prefiro não dizer</option>
+            </select>
+          </div>
           <Button type="submit">Cadastrar</Button>
         </Form>
-        <div />
-        {/* É necessário fazer um componente para essa parte de link, e cirar um outro
-          componente para esquecer a senha
-        */}
-        <div>
+        <div className="register-page__link">
           <p>
-            Já tem uma conta? <Link to="/">Faça login</Link>
+            Já tem uma conta? <Link to="/login">Faça login</Link>
           </p>
         </div>
       </div>
