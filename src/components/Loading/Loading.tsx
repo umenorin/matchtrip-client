07d148f96713a7765ddab21f980185fc26884286
lucas-dev98 <l.o.bastos@live.com>
2025-05-18 12:07:21 -0300

    feat : Adding Loading on page Login

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,9 @@
+import "./Loading.scss";
+
+export default function Loading() {
+  return (
+    <div className="loading-container">
+      <div className="loading"></div>
+    </div>
+  );
+}
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,11 +1,18 @@
 import "./LoginPage.scss";
-
 import { Link, Form, useActionData } from "react-router";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
+import Loading from "../../components/Loading/Loading";
+import { useState } from "react";
 
 const LoginPage = () => {
   const actionData = useActionData();
+  const [loading, setLoading] = useState(false);
+
+  function handleSubmit(e) {
+    setLoading(true);
+    // O Form do react-router vai continuar o submit normalmente
+  }
 
   return (
     <div className="login-page">
@@ -13,17 +20,20 @@ const LoginPage = () => {
         <h1 className="login-page__title">Faça o Login</h1>
       </div>
       <div className="login-page__form-div">
-        <Form className="login-page__form" method="post">
-          <Input name="email" type="email" placeholder="Email" />
-          {actionData?.errors?.email && <p>{actionData.errors.email}</p>}
+        {loading ? (
+          <Loading />
+        ) : (
+          <Form className="login-page__form" method="post" onSubmit={handleSubmit}>
+            <Input name="email" type="email" placeholder="Email" />
+            {actionData?.errors?.email && <p>{actionData.errors.email}</p>}
 
-          <Input name="password" type="password" placeholder="Senha" />
-          {actionData?.errors?.password && <p>{actionData.errors.password}</p>}
+            <Input name="password" type="password" placeholder="Senha" />
+            {actionData?.errors?.password && <p>{actionData.errors.password}</p>}
 
-          <Button type="submit">Entrar</Button>
-        </Form>
+            <Button type="submit">Entrar</Button>
+          </Form>
+        )}
       </div>
-      {/* Component para links (Recuperar senha, cadastrar) */}
       <p>
         Não tem uma conta? <Link to="/register">Cadastre-se</Link>
       </p>
