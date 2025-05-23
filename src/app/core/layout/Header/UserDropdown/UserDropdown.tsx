95b5fd0b0ee83b1505b7d4cc271826c10a2126cb
lucas-dev98 <l.o.bastos@live.com>
2025-05-23 18:43:47 -0300

    Feat : Logout on profile

--- src/app/core/layout/Header/UserDropdown/UserDropdown.tsx
+++ src/app/core/layout/Header/UserDropdown/UserDropdown.tsx
@@ -2,11 +2,10 @@ import { useState, useRef, useEffect } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import "./UserDropdown.scss";
-import Button from "../../../shared/Button/Button";
 import { useAuth } from "../../../../features/services/context/AuthContext";
 
 const UserDropdown = () => {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
   const navigate = useNavigate();
@@ -31,49 +30,53 @@ const UserDropdown = () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
-    console.log("USER DROPDOWN: ",user)
+  console.log("USER DROPDOWN: ", user);
   return (
-    <div className="user-dropdown" ref={dropdownRef}>
-      {user ? (
-        <>
-          <button
-            className="user-dropdown__toggle"
-            onClick={() => setIsOpen(!isOpen)}
-            aria-label="Menu do usuário"
-          >
-            {user?.profileImage ? (
-              <img
-                src={ import.meta.env.VITE_LOCAL_API+ user.profileImage}
-                alt="Foto do usuário"
-                className="user-dropdown__photo"
-              />
-            ) : (
-              <FaUserCircle className="user-dropdown__icon" />
-            )}
-          </button>
+    <div className="user-dropdown-wrapper">
+      <div className="user-dropdown" ref={dropdownRef}>
+        {user ? (
+          <>
+            <button
+              className="user-dropdown__toggle"
+              onClick={() => setIsOpen(!isOpen)}
+              aria-label="Menu do usuário"
+            >
+              {user?.profileImage ? (
+                <img
+                  src={import.meta.env.VITE_LOCAL_API + user.profileImage}
+                  alt="Foto do usuário"
+                  className="user-dropdown__photo"
+                />
+              ) : (
+                <FaUserCircle className="user-dropdown__icon" />
+              )}
+            </button>
 
-          {isOpen && (
-            <div className="user-dropdown__menu">
-              <button
-                className="user-dropdown__item"
-                onClick={handleProfileClick}
-              >
-                <FaUserCircle className="user-dropdown__item-icon" />
-                <span>Meu Perfil</span>
-              </button>
-            </div>
-          )}
-        </>
-      ) : (
-        <div className="perfil-none">
-          <Button navigateTo="/login" type="link">
-            Logar
-          </Button>
-          <Button navigateTo="/register" type="link">
-            Registrar
-          </Button>
-        </div>
-      )}
+            {isOpen && (
+              <div className="user-dropdown__menu">
+                <button
+                  className="user-dropdown__item"
+                  onClick={handleProfileClick}
+                >
+                  <FaUserCircle className="user-dropdown__item-icon" />
+                  <span>Meu Perfil</span>
+                </button>
+                <button
+                  className="user-dropdown__logout"
+                  onClick={() => {
+                    setIsOpen(false);
+                    logout();
+                  }}
+                >
+                  Sair
+                </button>
+              </div>
+            )}
+          </>
+        ) : (
+          <div className="perfil-none"></div>
+        )}
+      </div>
     </div>
   );
 };
--- src/app/features/HomePage/HomePage.tsx
+++ src/app/features/HomePage/HomePage.tsx
@@ -3,6 +3,7 @@ import CategoryFilter from "../../core/components/CategoryFilter/CategoryFilter"
 import "./HomePage.scss";
 import MyRequestsPage from "../MyRequestsPage/MyRequestsPage";
 
+
 export default function HomePage() {
   return (
     <div className="home-page">
