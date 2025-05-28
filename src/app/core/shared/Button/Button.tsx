9e23a9e16e394c3fd3f770ff067787612ef843bc
lucas-dev98 <l.o.bastos@live.com>
2025-05-28 13:28:44 -0300

    Feat: Adding button new trip when was user login is true

--- src/app/core/shared/Button/Button.tsx
+++ src/app/core/shared/Button/Button.tsx
@@ -6,14 +6,19 @@ type ButtonProps = {
   children: React.ReactNode;
   type: "button" | "link";
   navigateTo?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
-export default function Button({ children, type, navigateTo }: ButtonProps) {
+export default function Button({ children, type, navigateTo, onClick }: ButtonProps) {
   if (type === "link" && navigateTo)
     return (
       <Link to={navigateTo} className="button">
         {children}
       </Link>
     );
-  return <button className="button">{children}</button>;
+    return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
 }
